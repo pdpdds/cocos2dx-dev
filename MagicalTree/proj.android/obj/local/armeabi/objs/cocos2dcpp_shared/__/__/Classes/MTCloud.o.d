ba06e02db526d2f9061f614d5bc85953a52e3158
pdpdds <pdpdds@9eae94da-17f8-0410-9712-9035c0ba4dfd>
2014-07-31 12:15:44 +0000

    magicaltree

--- MagicalTree/proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/MTCloud.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/MTCloud.o: \
- jni/../../Classes/MTCloud.cpp jni/../../Classes/MTCloud.h
-
-jni/../../Classes/MTCloud.h:
--- MagicalTree/proj.android/obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/MTPlatform.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs/cocos2dcpp_shared/__/__/Classes/MTPlatform.o: \
- jni/../../Classes/MTPlatform.cpp jni/../../Classes/MTPlatform.h
-
-jni/../../Classes/MTPlatform.h:
